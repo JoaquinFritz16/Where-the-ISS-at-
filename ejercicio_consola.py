21df54774eec0af84634e67f6dff5beef02252a9
Joaquin Ignacio Fritz Carrizo <d48125437@instituto.itr.edu.ar>
2024-05-23 11:19:10 -0300

    Ejercicio terminado con velocidad

--- ejercicio_consola.py
+++ ejercicio_consola.py
@@ -6,8 +6,10 @@ iss=url.json()
 while True:
     iss_latitude=iss['latitude']
     iss_longitude=iss['longitude']
+    iss_speed=iss['velocity']
     maps = f'https://www.google.com/maps/@{iss_latitude},{iss_longitude},14.43z?entry=ttu'
     webbrowser.open(maps)
     print(iss_latitude)
     print(iss_longitude)
+    print(iss_speed)
     break
